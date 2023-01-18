36c682aa44689a3ea02ef9471555ddca86e89e92
Richard Folea <Folea3@gmail.com>
2023-01-18 08:23:01 -0500

    github actions

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  basePath = `/${repo}`
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.gltf','**/*.fbx','**/*.glb','**/*.bin'],
+  base: basePath,
 })
