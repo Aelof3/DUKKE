3ed238d232583e38525a3cac44d307d2dcbf08c5
Richard Folea <Folea3@gmail.com>
2023-01-24 13:34:34 -0500

    update cars to bounce instead of reset

--- src/components/levels/city/CityLevel2.jsx
+++ src/components/levels/city/CityLevel2.jsx
@@ -61,17 +61,17 @@ export default function CityLevel2(props) {
                 <Block args={[10, 25, buildingWidth]} position={[24.5, 12.5, -10]} color={"yellow"} />
             </RigidBody>
 
-            <RigidBody ref={carRef0} name={"car_reset"} position={[7,2,0]} type={"fixed"} colliders="cuboid">
+            <RigidBody ref={carRef0} name={"car"} position={[7,2,0]} type={"fixed"} colliders="cuboid">
                 <Block args={[7, 4, 5]} color={"black"}/>
             </RigidBody>
-            <RigidBody ref={carRef1} name={"car_reset"} position={[22,2,0]} type={"fixed"} colliders="cuboid">
+            <RigidBody ref={carRef1} name={"car"} position={[22,2,0]} type={"fixed"} colliders="cuboid">
                 <Block args={[7, 4, 5]} color={"black"}/>
             </RigidBody>
 
-            <RigidBody ref={carRef2} name={"car_reset"} position={[14.5,4,14.5]} rotation={[0,Math.PI/2,0]} type={"fixed"} colliders="cuboid">
+            <RigidBody ref={carRef2} name={"car"} position={[14.5,4,14.5]} rotation={[0,Math.PI/2,0]} type={"fixed"} colliders="cuboid">
                 <Block args={[12, 8, 5]} color={"black"}/>
             </RigidBody>
-            <RigidBody ref={carRef3} name={"car_reset"} position={[14.5,4,-0.5]} rotation={[0,Math.PI/2,0]} type={"fixed"} colliders="cuboid">
+            <RigidBody ref={carRef3} name={"car"} position={[14.5,4,-0.5]} rotation={[0,Math.PI/2,0]} type={"fixed"} colliders="cuboid">
                 <Block args={[12, 8, 5]} color={"black"}/>
             </RigidBody>
         </LevelBase>
