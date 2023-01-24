1366acb2c6802bc9c01b7f7db5bae99867f573db
Richard Folea <Folea3@gmail.com>
2023-01-23 21:08:36 -0500

    Update Engine.jsx

--- src/components/player/Engine.jsx
+++ src/components/player/Engine.jsx
@@ -4,12 +4,10 @@ export default function Engine(props) {
     const { toggle } = props
     
     const color = toggle ? "yellow" : "transparent"
-    const width = toggle ? 3 : 3
-    const length = toggle ? 2 : 2
     const decay = toggle ? 5 : 100
 
     return (
-        <Trail width={width} length={length} attenuation={(w) => w} decay={decay} color={color}>
+        <Trail width={3} length={2} attenuation={(w) => w} decay={decay} color={color}>
             <Box args={[0.01, 0.01, 0.01]} {...props} />
         </Trail>
     )
